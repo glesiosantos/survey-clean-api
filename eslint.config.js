69c4e86e3dc0b29487916fb572b7657bfe306164
Glêsio Santos <glesioss@gmail.com>
2025-06-13 09:45:08 -0300

    chore: add eslint and prettier

--- eslint.config.js
+++ eslint.config.js
@@ -4,29 +4,47 @@ import prettierPlugin from 'eslint-plugin-prettier'
 
 export default [
   {
-    ignores: ['node_modules', 'dist'],
+    ignores: ['node_modules', 'dist']
   },
   {
     files: ['**/*.ts', '**/*.tsx'],
     plugins: {
       '@typescript-eslint': tsPlugin,
-      prettier: prettierPlugin,
+      prettier: prettierPlugin
     },
     languageOptions: {
       parser: tsParser,
       parserOptions: {
         project: './tsconfig.json',
         sourceType: 'module',
-        ecmaVersion: 2021,
-      },
+        ecmaVersion: 2021
+      }
     },
     rules: {
-        'prettier/prettier': ['error', {semi: false, singleQuote: true, trailingComma: 'none'}],
-        '@typescript-eslint/explicit-function-return-type': 'off',
-        '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-        '@typescript-eslint/no-magic-numbers': 'off',
-        'indent': 'off',
-        'space-before-function-paren': 'off',
+      indent: 'off',
+      '@typescript-eslint/indent': 'off',
+      'prettier/prettier': [
+        'error',
+        {
+          semi: false,
+          singleQuote: true,
+          trailingComma: 'none',
+          tabWidth: 2,
+          useTabs: false
+        }
+      ],
+      '@typescript-eslint/explicit-function-return-type': [
+        'error',
+        {
+          allowExpressions: true,
+          allowTypedFunctionExpressions: true
+        }
+      ],
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_' }
+      ],
+      '@typescript-eslint/no-magic-numbers': 'off'
     }
-  },
+  }
 ]
