8cdedf370e8e57a70e70c725e4271f1dea54da7b
Glêsio Santos <glesioss@gmail.com>
2025-06-22 13:05:11 -0300

    refactor: ensure SignUpController.handle implement correct interfaces

--- src/presentation/controllers/signup_controller.spec.ts
+++ src/presentation/controllers/signup_controller.spec.ts
@@ -1,9 +1,10 @@
+import { HttpRequest, HttpResponse } from '../protocols/http'
 import { SignUpController } from './signup_controller'
 
 describe('SignUp Controller', () => {
   it('should return 400 when name is not provided', () => {
     const sut = new SignUpController()
-    const httpRequest = {
+    const httpRequest: HttpRequest = {
       body: {
         email: 'any_email@mail.com',
         password: 'any_password',
@@ -11,14 +12,14 @@ describe('SignUp Controller', () => {
       }
     }
 
-    const httpResponse = sut.handle(httpRequest)
+    const httpResponse: HttpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing param: name'))
   })
 
   it('should return 400 when email is not provided', () => {
     const sut = new SignUpController()
-    const httpRequest = {
+    const httpRequest: HttpRequest = {
       body: {
         name: 'any_name',
         password: 'any_password',
