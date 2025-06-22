a7efa9cf28eeb79adaa9c19e59bae8b8902818c4
Glêsio Santos <glesioss@gmail.com>
2025-06-22 13:30:01 -0300

    test: ensure SignUpControllere return a error when a passwordConfirmation is not provided

--- src/presentation/controllers/signup_controller.spec.ts
+++ src/presentation/controllers/signup_controller.spec.ts
@@ -45,4 +45,20 @@ describe('SignUp Controller', () => {
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse).toEqual(badRequest(new MissingParamError('password')))
   })
+
+  it('should return 400 when passwordConfirmation is not provided', () => {
+    const sut = new SignUpController()
+    const httpRequest: HttpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password'
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse).toEqual(
+      badRequest(new MissingParamError('passwordConfirmation'))
+    )
+  })
 })
