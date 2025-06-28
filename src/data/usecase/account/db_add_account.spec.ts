cf264675c3ca593e9a2ec3f87144269c3bf7f2df
Glêsio Santos <glesioss@gmail.com>
2025-06-28 12:29:36 -0300

    test: move sut creation to a factory helper method

--- src/data/usecase/account/db_add_account.spec.ts
+++ src/data/usecase/account/db_add_account.spec.ts
@@ -1,16 +1,27 @@
+import { AddAccount } from '../../../domain/usecases/account/add_account'
 import { DBAddAccount } from '../../db/db_add_account'
 import { Encrypter } from '../../protocol/encrypter'
 
+type SutTypes = {
+  encrypterStub: Encrypter
+  sut: AddAccount
+}
+
+const makeSut = (): SutTypes => {
+  class EncrypterStub implements Encrypter {
+    encrypt(value: string): Promise<string> {
+      return new Promise((resolve) => resolve('hashed_password'))
+    }
+  }
+  const encrypterStub = new EncrypterStub()
+  const sut = new DBAddAccount(encrypterStub)
+
+  return { sut, encrypterStub }
+}
+
 describe('DDAddAccount usecase', () => {
   it('should call Encrypter with correct password', () => {
-    class EncrypterStub implements Encrypter {
-      encrypt(value: string): Promise<string> {
-        return new Promise((resolve) => resolve('hashed_password'))
-      }
-    }
-
-    const encrypterStub = new EncrypterStub()
-    const sut = new DBAddAccount(encrypterStub)
+    const { sut, encrypterStub } = makeSut()
 
     const accountData = {
       name: 'any_name',
