41cd9f940cf45623f377320e0ad60e370576636d
Glêsio Santos <glesioss@gmail.com>
2025-06-29 10:44:34 -0300

    refactor: ajuste script jest config

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,9 +1,12 @@
 /** @type {import('jest').Config} */
 const config = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts', 
-    '!<rootDir>/src/presentation/controllers/**/*_protocol.ts',
-    '!**/protocols/**'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/domain/**/**.ts', 
+    '!<rootDir>/src/**/**/**/*_protocol.ts',
+    '!**/protocols/**'
+  ],
   coverageDirectory: "coverage",
   coverageProvider: "v8",
   transform: {
