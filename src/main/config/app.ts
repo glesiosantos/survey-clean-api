1c0076b6e5a33891e1454ba9bd621f8f8fa91702
Glêsio Santos <glesioss@gmail.com>
2025-07-03 17:10:57 -0300

    feat: ensure return an account on success

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,7 +1,9 @@
 import express from 'express'
 import setupMiddlewares from './middlewares.ts'
+import setupRoutes from './routes.ts'
 
 const app = express()
 setupMiddlewares(app)
+setupRoutes(app)
 
 export default app
