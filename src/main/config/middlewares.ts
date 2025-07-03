fa5c10140a8b701ea26f1a0af7a0ee9633d65a97
Glêsio Santos <glesioss@gmail.com>
2025-07-03 16:28:15 -0300

    feat: ensure enabled cors

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,6 +1,8 @@
 import { Express } from 'express'
 import { bodyParser } from '../middlewares/body_parser.ts'
+import { cors } from '../middlewares/cors.ts'
 
 export default (app: Express): void => {
   app.use(bodyParser)
+  app.use(cors)
 }
